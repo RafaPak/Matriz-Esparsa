28132921fd013a1343a6c73fec1c1024a2f06ee6
Rafael Pak Bragagnolo <42739598+RafaPak@users.noreply.github.com>
2019-04-04 09:06:16 -0300

    Add files via upload

---
+++ Celula.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApMatrizEsparsa
+{
+    public class Celula
+    {
+        Celula direita, abaixo;
+        int linha, coluna;
+        double valor;
+
+        public Celula(int l, int c, double v)
+        {
+            linha = l;
+            coluna = c;
+            valor = v;
+        }
+
+        public double Valor { get => valor; set => valor = value; }
+        public int Linha { get => linha; set => linha = value; }
+        public int Coluna { get => coluna; set => coluna = value; }
+        public Celula Direita { get => direita; set => direita = value; }
+        public Celula Abaixo { get => abaixo; set => abaixo = value; }
+
+        public override string ToString()
+        {
+            return "( " + this.Valor + "; " + this.Linha + "; " + this.Coluna + " )";
+        }
+    }
+}
---
+++ frmME.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ApMatrizEsparsa
+{
+    public partial class frmME : Form
+    {
+        private Label lbTitulo;
+
+        public frmME()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            this.lbTitulo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbTitulo
+            // 
+            this.lbTitulo.AutoSize = true;
+            this.lbTitulo.Font = new System.Drawing.Font("Wide Latin", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTitulo.Location = new System.Drawing.Point(50, 9);
+            this.lbTitulo.Name = "lbTitulo";
+            this.lbTitulo.Size = new System.Drawing.Size(561, 26);
+            this.lbTitulo.TabIndex = 0;
+            this.lbTitulo.Text = "Operações com Matriz Esparsa";
+            this.lbTitulo.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // frmME
+            // 
+            this.ClientSize = new System.Drawing.Size(685, 398);
+            this.Controls.Add(this.lbTitulo);
+            this.Name = "frmME";
+            this.Text = "Matriz Esparsa";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
